aa1ddb040f9109c3ad5251b45fb80c1fd9698cbf
androllen <androllen@hotmail.com>
2015-09-28 17:37:58 +0800

    点击屏幕 生成控件 拖动 缩放 旋转 删除功能初始化

---
+++ app/src/main/java/com/cc/imagewithmarkersample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cc.imagewithmarkersample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty_main);
+    }
+
+
+}
